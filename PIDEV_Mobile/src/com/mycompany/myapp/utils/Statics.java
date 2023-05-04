312868e554bf9771d0cad43f0a469761a745dcec
Mahmoud Mzoughi <mahmoud.mzoughi@esprit.tn>
2023-05-04 01:04:12 +0100

    BUILD 04052023
    First Mobile push
    
    Full ALLREC+REC+REPONSE+ADD

---
+++ PIDEV_Mobile/src/com/mycompany/myapp/utils/Statics.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.myapp.utils;
+
+/**
+ *
+ * @author bhk
+ */
+public class Statics {
+    //public static final String BASE_URL="https://workshopparsingjsoncn1back-production.up.railway.app/task/";
+    public static final String BASE_URL="http://127.0.0.1:8000/reclamationJSON";
+    public static final String RECBYUSERID_URL="http://127.0.0.1:8000/reclamationsParUserIdJSON";
+    public static final String RECBYRECID_URL="http://127.0.0.1:8000/reclamationParRecIdJSON";
+    
+    public static final String NEWREP_URL="http://127.0.0.1:8000/reclamationRepJSON";
+    public static final String FETSHREPS_URL="http://127.0.0.1:8000/reponsesParRecIdJSON";
+    
+    
+    public static final String RECPLUSREPBYRECID_URL="http://127.0.0.1:8000/reclamationEtReponsesParRecIdJSON";
+    
+    
+    
+}
