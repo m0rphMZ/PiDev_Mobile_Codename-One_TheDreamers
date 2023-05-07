b3baac026485a084efe8a8fe687e52ff09724289
Mantassar <80499945+Mantassar@users.noreply.github.com>
2023-05-07 14:40:01 +0100

    Add files via upload
    commit1

---
+++ MobileeMonta/src/com/mycompany/myapp/gui/Ajouter.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.myapp.gui;
+
+import com.codename1.ui.Image;
+import java.io.IOException;
+
+/**
+ *
+ * @author Monta
+ */
+public class Ajouter {
+    Image myImage;
+
+    public Ajouter() throws IOException {
+        this.myImage = Image.createImage("../src/back-logo.jpeg");
+    }
+
+
+ 
+}
